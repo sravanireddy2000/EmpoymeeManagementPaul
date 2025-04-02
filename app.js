84d117060b51701c6ff584d301f8ac1ecd58a774
Upputuris16@gmail.com <Upputuris16@gmail.com>
2025-04-02 22:48:25 +0530

    viiews

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const dotenv=require('dotenv').config({path:'./.env.development'});
 const app=express();
 const database=require('./config/database.js');
 const path = require('path');
+const ejs=require('ejs');
 
 database();
-
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname,'static')));
+
 
 app.listen('2000',()=>console.log(`Server running on port ${process.env.PORT}`))
