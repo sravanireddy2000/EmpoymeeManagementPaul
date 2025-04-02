a991fe46a06054a61ccec6c3575606865e40ed75
Upputuris16@gmail.com <Upputuris16@gmail.com>
2025-04-02 22:45:13 +0530

    staticfolderpath

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express=require('express');
 const dotenv=require('dotenv').config({path:'./.env.development'});
 const app=express();
 const database=require('./config/database.js');
+const path = require('path');
 
 database();
 
+app.use(express.static(path.join(__dirname,'static')));
 
 app.listen('2000',()=>console.log(`Server running on port ${process.env.PORT}`))
