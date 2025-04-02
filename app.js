b02329986c61c0c9f1d34c85ad93154df58f1f46
Upputuris16@gmail.com <Upputuris16@gmail.com>
2025-04-02 22:58:46 +0530

    router

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app=express();
 const database=require('./config/database.js');
 const path = require('path');
 const ejs=require('ejs');
+const router=require('./routes/index.js');
 
 database();
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname,'static')));
 
+app.use('/',router);
 
 app.listen('2000',()=>console.log(`Server running on port ${process.env.PORT}`))
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const router=express.Router();
+
+
+router.get('/login',(req,res)=>{
+
+});
+
+router.post('/login',(req,res)=>{
+    
+});
+
+router.get('/register',(req,res)=>{
+
+});
+
+router.post('/register',(req,res)=>{
+    
+})
+
+module.exports=router;
