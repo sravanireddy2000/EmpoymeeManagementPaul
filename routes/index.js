394331a28a4ab5ed4f1a5bd29662df4668d0b046
Upputuris16@gmail.com <Upputuris16@gmail.com>
2025-04-03 22:10:13 +0530

    added register route

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,19 @@ router.get('/register',(req,res)=>{
     res.render('register.ejs')
 });
 
-router.post('/register',(req,res)=>{
+router.post('/register',async (req,res)=>{
     const body=req.body;
+   try{
+    const update=await registerModel.create({name:body.name,email:body.email,password:body.password})
+    if(!update)
+        return res.status(400).json({success:false,message:'Unable to register'});
+    return res.status(201).json({success:true,message:'Registered successfully'})
+   }
+   catch(err)
+   {    return res.status(500).json({success:false,message:'Network error'});
+
+   }
+ 
 })
 
 module.exports=router;
