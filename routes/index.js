f29d1d2b30a1e85a0af776a8489fa17e089c40f5
Upputuris16@gmail.com <Upputuris16@gmail.com>
2025-04-03 22:18:55 +0530

    login page_regsiter user_completed

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,27 @@ const registerModel=require('../models/register.js');
 router.get('/',(req,res)=>{
    res.render('login.ejs')
 });
-
-router.post('/login',(req,res)=>{
+router.get('/homepage',(req,res)=>{
+    res.render('index.ejs')
+ });
+ 
+router.post('/login',async (req,res)=>{
      const body=req.body;
+     try{
+      const update=await registerModel.findOne({email:body.email})
+ 
+      if(!update)
+          return res.status(400).json({success:false,message:'Unable to find User'});
      
+      if(body.password!==update.password)   
+        return res.status(400).json({success:false,message:'Password not match'});
+
+      return res.status(200).json({success:true,message:'User identified'});
+     }
+     catch(err)
+     {   
+       return res.status(500).json({success:false,message:'Network error'});
+     }
 });
 
 router.get('/register',(req,res)=>{
@@ -24,10 +41,9 @@ router.post('/register',async (req,res)=>{
     return res.status(201).json({success:true,message:'Registered successfully'})
    }
    catch(err)
-   {    return res.status(500).json({success:false,message:'Network error'});
-
+   {   
+     return res.status(500).json({success:false,message:'Network error'});
    }
- 
 })
 
 module.exports=router;
